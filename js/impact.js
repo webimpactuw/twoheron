f52c90e5b79dbe0072e972012168cf6530c98d6b
“Hannah <Hannah>
2022-05-15 15:58:45 -0700

    Attempting carousel

--- js/impact.js
+++ js/impact.js
@@ -1,12 +1,26 @@
-let container = document.querySelector('.card-container');
-let left = document.querySelector('.left');
-let right = document.querySelector('.right');
-let card = document.querySelector('.card');
-(function () {
-    right.addEventListener('click', function (e) {
-       container.scrollLeft += item.clientWidth;
-    });
-    left.addEventListener('click', function () {
-       container.scrollLeft -= item.clientWidth;
-    })
+let slider = tns({
+   container: ".my-slider", // What's the slider class name
+   "slideBy": 1, // How many slides are we skipping per press?
+   "speed": 400, //speed of slide
+   "nav" : false,  // no dots
+   controlsContainer: "#controls", //We want to use our controls rather than their
+   prevButton: ".previous", //set buttons
+   nextButton: ".next",
+   responsive: { //images to display based on window size
+      1600: {
+         items: 4, //# images
+         gutter: 20 //padding between images
+      },
+      1024: {
+         items: 3,
+         gutter: 20
+      },
+      768: {
+         items: 2,
+         gutter: 20
+      },
+      480: {
+         items: 1,
+      }
+   }
 });
